c1091050ee959f679ef96687badf44000f018a11
pryahin <pryahin.v@gmail.com>
2016-09-22 22:16:51 +0300

    lab1

---
+++ labs/lab1/examples/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <print_hello_world.h>
+
+int main()
+{
+	hello_world();
+	return 0;
+}
---
+++ labs/lab1/sources/print_hello_world.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <print_hello_world.h>
+
+void hello_world()
+{
+	std::cout << "Hello World!" << std::endl;
+}
